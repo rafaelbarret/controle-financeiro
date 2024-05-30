43bf069f60b29ce392af247269c969663ed8ad2c
rafaelbarret <rafaelferreiraproductions@gmail.com>
2024-05-30 07:58:14 -0300

    App de controle financeiro
    # Aplicativo de Controle Financeiro
    
    Este é um aplicativo de controle financeiro desenvolvido para ajudar os usuários a gerenciar suas finanças pessoais de maneira eficiente. Com uma interface de usuário intuitiva e fácil de usar, o aplicativo permite que os usuários rastreiem suas despesas.
    
    Desenvolvido em ReactJs e utilizando localStorage como armazenamento

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import GlobalStyle from './styles/global'
+import Header from './components/Header';
+import Resume from './components/Resume';
+import Form from './components/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App = () => {
+  // Lógica para o somatório do resultado da operação
+  const data = localStorage.getItem("transactions");
+  const [transactionsList, setTransactionsList] = useState(
+    data ? JSON.parse(data): []
   );
-}
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [total, setTotal] = useState(0);
 
-export default App;
+  useEffect(() => {
+    const amountExpense = transactionsList
+      .filter((item) => item.expense)
+      .map((transaction) => Number(transaction.amount));
+    
+    const amountIncome = transactionsList
+    .filter((item) => !item.expense)
+    .map((transaction) => Number(transaction.amount));
+
+    const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+    const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+
+    const total = Math.abs(income - expense).toFixed(2);
+
+    setIncome(`R$ ${income}`);
+    setExpense(`R$ ${expense}`);
+    setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`);//Aqui mostra o cálculo se ficar negativo, posteriormente podemos colocar para ficar na cor vermelha o resultado
+
+  }, [transactionsList]);
+//Vêm até aqui o resultado da operação ##########
+
+//Aqui abaixo permite adicionar novos items
+  const handleAdd = (transaction) => {
+    const newArrayTransactions = [...transactionsList, transaction];
+
+    setTransactionsList(newArrayTransactions);
+
+    localStorage.setItem("transactions", JSON.stringify(newArrayTransactions));
+  };
+
+  return (
+    <>
+        <Header />
+        <Resume income={income} expense={expense} total={total}/>
+        <Form />
+        <GlobalStyle />
+    </>
+ );
+};
+
+export default App
---
+++ src/components/Form/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import * as C from "./styles";
+
+const Form = () => {
+  const [desc, setDesc] = useState("");
+  const [amount, setAmount] = useState("");
+  const [isExpense, setExpense] = useState(false);
+
+  const handleSave = () => {
+    if (!desc || !amount) {
+      alert("informe a descrição e o valor!");
+      return;
+    } else if (amount < 1){
+      alert("o valor tem que ser positivo!");
+    }
+  };
+
+  return (
+    <>
+      <C.Container>
+          <C.InputContent>
+              <C.Label>Descrição</C.Label>
+              <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
+          </C.InputContent>
+          <C.InputContent>
+              <C.Label>Valor</C.Label>
+              <C.Input 
+                value={amount} 
+                type="number"
+                onchange={(e) => 
+                setAmount(e.target.value)}
+                />
+          </C.InputContent>
+          <C.RadioGroup>
+              <C.Input
+              type="radio"
+              id="rIncome"
+              defaultChecked
+              name="group1"
+              onChange={() => setExpense(!isExpense)} 
+              />
+              <C.Label htmlFor='rIncome'>Entrada</C.Label>
+              <C.Input
+              type="radio"
+              id="rExpenses"
+              name="group1"
+              onChange={() => setExpense(!isExpense)} 
+              />
+              <C.Label htmlFor='rExpenses'>Saídas</C.Label>
+          </C.RadioGroup>
+          <C.Button onClick={handleSave}>ADICIONAR</C.Button>
+      </C.Container>
+    </>
+  );
+};
+
+export default Form;
